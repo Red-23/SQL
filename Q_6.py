1a1c83324e665f1852e783913bef34f67bdbec98
Red <0djamb17@solent.ac.uk>
2026-03-24 15:54:11 +0000

    Practice questions complete

--- Q_6.py
+++ Q_6.py
@@ -1,3 +1,16 @@
 import sqlite3
 db_file = "C:/Users/bilan/Downloads/mysports.db"
 db = sqlite3.connect(db_file)
+cursor = db.cursor()
+sql_query = ("SELECT ename, name "\
+            "FROM customer c "\
+            "   INNER JOIN emp e ON c.repid = e.empno "
+             "ORDER BY ename")
+cursor.execute(sql_query)
+all_rows = cursor.fetchall()
+print(f"{'Sales Rep':10}\t{'Customer Name':40}\n")
+for row in all_rows:
+    repname = row[0]
+    custname = row[1]
+    print(f"{repname:10}\t{custname}")
+db.close()
